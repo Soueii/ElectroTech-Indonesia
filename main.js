4dcde3fc46cf873995e528aef45cd9b1c083008d
Soueii <halifex16@gmail.com>
2024-09-02 16:48:43 +0300

    Add fully working menu with hamburger animation.

--- main.js
+++ main.js
@@ -1,5 +1,22 @@
-var btn = document.querySelector(".btn");
+let btn = document.querySelector(".btn");
 
 btn.addEventListener("click", function () {
   this.classList.toggle("active");
+
+  let menuList = document.querySelector(".menu-list");
+  let displayState = menuList.style.display;
+
+  if (displayState === "none" || displayState === "") {
+    menuList.style.display = "block";
+  } else {
+    menuList.style.display = "none";
+  }
+
+  if (menuList.classList.contains("show")) {
+    menuList.classList.remove("show");
+    menuList.classList.add("hide");
+  } else {
+    menuList.classList.remove("hide");
+    menuList.classList.add("show");
+  }
 });
