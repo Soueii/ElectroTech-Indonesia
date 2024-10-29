8cacdc0f1749df7a7f1adbd46540141592e66647
Soueii <halifex16@gmail.com>
2024-10-29 19:30:28 +0200

    Completely change the Menu animation. Only index page is left to fix.

--- main.js
+++ main.js
@@ -1,28 +1,3 @@
-// Menu animation button
-
-let btn = document.querySelector(".btn");
-
-btn.addEventListener("click", function () {
-  this.classList.toggle("active");
-
-  let menuList = document.querySelector(".menu-list");
-  let displayState = menuList.style.display;
-
-  if (displayState === "none" || displayState === "") {
-    menuList.style.display = "block";
-  } else {
-    menuList.style.display = "none";
-  }
-
-  if (menuList.classList.contains("show")) {
-    menuList.classList.remove("show");
-    menuList.classList.add("hide");
-  } else {
-    menuList.classList.remove("hide");
-    menuList.classList.add("show");
-  }
-});
-
 // Accordion
 
 const accordions = document.querySelectorAll(".accordion");
